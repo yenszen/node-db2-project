78d8fb2e4a6856d345e0cd1a0d5148202e2113cc
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-05-26 19:39:11 +0800

    completed UPDATE and DELETE stretch tasks

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -18,7 +18,14 @@ router.get("/:VIN", async (req, res) => {
 
   try {
     const car = await db("cars").where({ VIN });
-    res.status(200).json(car);
+
+    if (car.length > 0) {
+      res.status(200).json(car);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The car with specified VIN does not exist" });
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Databbase error" });
@@ -31,6 +38,43 @@ router.post("/", async (req, res) => {
   try {
     const newCar = await db("cars").insert(carInfo);
     res.status(201).json(newCar);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(400)
+      .json({ message: "Please include make, model and mileage information" });
+  }
+});
+
+router.put("/:VIN", async (req, res) => {
+  const { VIN } = req.params;
+  const changes = req.body;
+
+  try {
+    const updated = await db("cars")
+      .where({ VIN })
+      .update(changes);
+
+    res.status(200).json(updated);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Database error" });
+  }
+});
+
+router.delete("/:VIN", async (req, res) => {
+  const { VIN } = req.params;
+
+  try {
+    const deleted = await db("cars")
+      .where({ VIN })
+      .del();
+
+    deleted
+      ? res.status(200).json({ message: "Car info removed" })
+      : res
+          .status(404)
+          .json({ message: "Car with specified VIN does not exist" });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Databbase error" });
